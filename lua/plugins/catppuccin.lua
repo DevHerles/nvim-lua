f946db7389a2fb3f0db10f464c0af1f2f76488fb
HerlesINC <herles.incalla@gmail.com>
2023-11-17 16:09:47 -0500

    First commit

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,10 @@
+return {
+	"catppuccin/nvim",
+  name = "catppuccin",
+	lazy = false,
+	priority = 999,
+	config = function()
+		vim.cmd('colorscheme catppuccin')
+	end
+}
+
---
+++ lua/plugins/lualine-nvim.lua
@@ -0,0 +1,23 @@
+local config = function()
+
+	require("lualine").setup({
+		options = {
+			theme = "catppuccin",
+			globalstatus = true,
+		},
+		tabline = {
+			lualine_a = { "mode" },
+			lualine_b = { "buffers" },
+			lualine_x = { "encoding", "fileformat", "filetype" },
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
+		},
+		sections = {},
+	})
+end
+
+return {
+	"nvim-lualine/lualine.nvim",
+	lazy = false,
+	config = config,
+}
